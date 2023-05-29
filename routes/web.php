d4312710f8ff407afb7122c86810546650d41b78
marcomazzotti <marcoalbertomazzotti@gmail.com>
2023-05-29 19:15:09 +0200

    Add: function index in ComicController + route

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\Guest\ComicController;
 use App\Http\Controllers\Guest\HomeController;
 use Illuminate\Support\Facades\Route;
 
@@ -15,3 +16,4 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', [HomeController::class, "index"])->name("home");
+Route::resource('comics', ComicController::class);
